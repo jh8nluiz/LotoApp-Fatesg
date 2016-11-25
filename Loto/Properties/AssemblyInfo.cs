c86ff042a762f29f6b4a145105a135954fb04388
jh8nluiz <jh8n.luiz@gmail.com>
2016-11-25 20:43:51 -0200

    Adicionado projeto UWP

--- Loto/Properties/AssemblyInfo.cs
+++ Loto/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Exemplo1")]
+[assembly: AssemblyTitle("Loto")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Exemplo1")]
+[assembly: AssemblyProduct("Loto")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
