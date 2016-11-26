99af2ecbe9db6a12f9f7267b1582741134c76474
Gustavo <ggomesdev@gmail.com>
2016-11-26 15:11:58 -0200

    Ajustes das importações

--- Loto/JsonWebClient.cs
+++ Loto/JsonWebClient.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
 using System.Threading.Tasks;
 
 namespace Loto
 {
-    class JsonWebClient    
+    class JsonWebClient
     {
         public async Task<System.IO.TextReader> DoRequestAsync(WebRequest req)
         {
