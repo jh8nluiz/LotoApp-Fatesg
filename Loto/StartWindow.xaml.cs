6fb86be569c0410f12aa9a14a9ebd11103fb4d87
Gustavo Fe <ggomesdev@gmail.com>
2016-12-02 15:08:24 -0800

    Busca pelos jogos via JSON finalizada

--- Loto/StartWindow.xaml.cs
+++ Loto/StartWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,7 +13,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Loto.Entity;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,21 +23,73 @@ namespace Loto
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private List<Jogo> listJogos = new List<Jogo>();
+        private JsonWebClient client = new JsonWebClient();
+        private ObservableCollection<Entity.Jogo> listJogos = new ObservableCollection<Entity.Jogo>();
+        private string token = "SGBWpLfcyvUskNU";
 
         public MainPage()
         {
-            this.InitializeComponent();
-
-            listJogos.Add(new Entity.Jogo { TipoJogo = "MEGA SENA", Resultado = "01 - 02 - 03 - 04 - 05 - 06" });
-            listJogos.Add(new Entity.Jogo { TipoJogo = "QUINA", Resultado = "01 - 02 - 03 - 04 - 05" });
-
+            this.InitializeComponent();            
             JogosList.ItemsSource = listJogos;
+            
         }
 
         private void btnLotoFacil_Click(object sender, RoutedEventArgs e)
-        {         
-            
+        {
+            buscarJogo("lotofacil");
         }
+
+       
+        private void btnMegaSena_Click(object sender, RoutedEventArgs e)
+        {            
+            buscarJogo("megasena");
+        }
+
+        private void btnLotoMania_Click(object sender, RoutedEventArgs e)
+        {
+            buscarJogo("lotomania");
+        }
+
+        private void btnDuplaSena_Click(object sender, RoutedEventArgs e)
+        {
+            buscarJogo("duplasena");
+        }
+
+        private void btnQuina_Click(object sender, RoutedEventArgs e)
+        {
+            buscarJogo("quina");
+        }
+
+        private void btnTimeMania_Click(object sender, RoutedEventArgs e)
+        {
+            buscarJogo("timemania");
+        }
+
+        private async void buscarJogo(string tipoJogo)
+        {
+            string url = "http://confiraloterias.com.br/api/json/?loteria=" + tipoJogo + "&token=" + token;
+            Entity.Jogo jogo = await client.DoRequestJsonAsync<Entity.Jogo>(url);
+            jogo.Resultado = tratarResultado(tipoJogo, jogo.concurso);
+            jogo.TipoJogo = tipoJogo.ToUpper();
+
+            listJogos.Clear();
+            listJogos.Add(jogo);
+        }        
+
+        private string tratarResultado(string tipoJogo, Entity.Concurso concurso)
+        {
+
+            if (tipoJogo == "duplasena")
+            {
+                return concurso.dezenas_1.Aggregate((current, next) => current + " - " + next);
+            }else
+            {
+                return concurso.dezenas.Aggregate((current, next) => current + " - " + next);
+            }
+
+
+        }
+
+        
     }
 }
